248c10497645f39c734222c5b5a3fb7acf97d5b3
HerbstblattMK <82040910+HerbstblattMK@users.noreply.github.com>
2022-10-11 13:20:20 +0200

    Add files via upload

--- snake/run.py
+++ snake/run.py
@@ -1,19 +1,48 @@
 import random
+import pygame
 from snake import SnakeGame
 
-snake = SnakeGame()
+environment = SnakeGame()
 
-@snake.register_ai
+@environment.register_ai
 def super_ai():
-    return [random.choice(['up', 'down', 'left', 'right'])]
 
-snake.start(use_ai=True)
+    apple = environment.get_apple_position()
+    snake = environment.get_snake_head_position()
+
+    if(snake[0] < apple[0]): #Go right if move is legal
+        if environment.is_legal('right'): #Check if move is legal
+            return 'right'
+    if(snake[0] > apple[0]): #Go left if move is leg  al
+        if environment.is_legal('left'): #Check if move is legal
+            return 'left'
+    if(snake[1] > apple[1]): #Go up if move is leg  al
+        if environment.is_legal('up'): #Check if move is legal
+            return 'up'
+    if(snake[1] < apple[1]): #Go down if move is leg  al
+        if environment.is_legal('down'): #Check if move is legal
+            return 'down'
+    
+    if environment.is_legal('right'): #Check if move is legal
+            return 'right'
+    if environment.is_legal('left'): #Check if move is legal
+            return 'left'
+    if environment.is_legal('up'): #Check if move is legal
+            return 'up'
+    if environment.is_legal('down'): #Check if move is legal
+            return 'down'
+    
+
+    
+
+environment.start(use_ai=True)
 
 
 
 #import pygame
 #from ikt111_games import Snake
 #import random
+
 #env = Snake()
 #@environment.register_ai
 #def super_ai():
